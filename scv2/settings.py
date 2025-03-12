b443f8bb394b100b0cf185e7a64a72967c02006d
Vitor Veras <vitor.veras@cee.ce.gov.br>
2025-03-12 16:27:06 -0300

    Assets

--- scv2/settings.py
+++ scv2/settings.py
@@ -187,9 +187,9 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATICFILES_DIRS = ['static',]
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
